a000ff29d0312beb3472c5cff8486000c1112212
rigginsn7 <47797012+rigginsn7@users.noreply.github.com>
2019-10-15 21:16:31 -0400

    Add files via upload

--- Button Blink/button blink g2/main.c
+++ Button Blink/button blink g2/main.c
@@ -1,3 +1,10 @@
+/* Author : Nicholas Riggins
+ * Date: September 2019
+ * Button blink code lab 2
+ * board MSP430G2553
+ * Will blink LED 1.0 when button 1.3 is pressed
+ *
+ */
 #include <msp430.h> 
 
 
@@ -8,17 +15,17 @@ int main(void)
 	
 
 
-	        P1DIR = BIT0;
+	        P1DIR = BIT0;      //  Clear P1.0 output latch for a defined power-on state
 
-	        P1REN = BIT3;
+	        P1REN = BIT3;     // Enable internal resistor so it can connect to 3.6v or ground
 
-	        P1OUT = BIT3;
+	        P1OUT = BIT3;     //Sets pin 1.3 as an input pin
+	                          // The LED initial value is at 1
 
 
 
-
-	            while( 1 ) {
-	                    if((P1IN & BIT3) !=BIT3)
+	            while( 1 ) {                       // Keeps the loop running
+	                    if((P1IN & BIT3) !=BIT3)  // if the value of pin input and bit 3 doesn't equal the value of bit 3  the LED will toggle
 	                    {
 
 	                    __delay_cycles(180000);
