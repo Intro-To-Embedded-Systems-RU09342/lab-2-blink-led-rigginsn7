d080bf01b8c6fe0f3bab6ae3e2c1be32668d5644
rigginsn7 <47797012+rigginsn7@users.noreply.github.com>
2019-10-15 21:16:00 -0400

    Add files via upload

--- Multiple Blink/multiple blink g2/main.c
+++ Multiple Blink/multiple blink g2/main.c
@@ -1,9 +1,14 @@
-#include <msp430.h> 
-
-
-/**
- * main.c
+/* Author : Nicholas Riggins
+ * Date: September 2019
+ * Multiple blink code lab 2
+ * board MSP430G2553
+ * Will blink led 1.0 and 2.5
+ *
  */
+
+#include <msp430.h>
+
+
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
@@ -13,11 +18,11 @@ int main(void)
 	        P1DIR |= BIT0;                          // Clear P1.0 output latch for a defined power-on state
 	        P1OUT |= BIT0;                          // Set P1.0 to output direction
 
-	        P2DIR |= BIT5;
-	        P2OUT |= BIT5;
+	        P2DIR |= BIT5;                          // Clear P2.5 output latch for a defined power-on state
+	        P2OUT |= BIT5;                          // Set P2.5 to output direction
 
 
-	        while(1)
+	        while(1)               // keeps program in a loop
 	        {
 	            P1OUT ^= BIT0;                      // Toggle LED
 	            __delay_cycles(100000);
