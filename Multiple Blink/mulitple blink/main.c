da153dfe68f9a9f138da61f51c6ee7688220e416
rigginsn7 <47797012+rigginsn7@users.noreply.github.com>
2019-10-15 21:15:13 -0400

    Add files via upload

--- Multiple Blink/mulitple blink/main.c
+++ Multiple Blink/mulitple blink/main.c
@@ -1,10 +1,14 @@
+/* Author : Nicholas Riggins
+ * Date: September 2019
+ * Multiple blink code lab 2
+ * board MSP-EXPFR6989
+ * Will blink led 1.0 and 9.7
+ *
+ */
+
 #include <msp430.h> 
 #include <stdio.h>
 
-
-/**
- * main.c
- */
 
 // Mulitple blink
 int main(void)
@@ -15,13 +19,13 @@ int main(void)
 	    P1DIR |= BIT0;                          // Clear P1.0 output latch for a defined power-on state
 	    P1OUT |= BIT0;                          // Set P1.0 to output direction
 
-	    P9DIR |= BIT7;
-	    P9OUT |= BIT7;
+	    P9DIR |= BIT7;                          //  Clear P9.7 output latch for a defined power-on state
+	    P9OUT |= BIT7;                          //  Set P9.7 to output direction
 
 	    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
 	                                            // to activate previously configured port settings
 
-	    while(1)
+	    while(1)                             // Keeps program in a loop
 	    {
 	        P1OUT ^= BIT0;                      // Toggle LED
 	        __delay_cycles(100000);
